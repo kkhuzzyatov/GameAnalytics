327bf20acf8bd5d74f4f104ca4b0b5555ac58e9b
perzhul <perzhul.works@gmail.com>
2025-06-08 19:43:19 +0300

    chore: add gin and base env config

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	Config struct {
+		App     App
+		HTTP    HTTP
+		Log     Log
+		PG      PG
+		Swagger Swagger
+	}
+
+	App struct {
+		Name    string `env:"APP_NAME,required"`
+		Version string `env:"APP_VERSION,required"`
+	}
+
+	HTTP struct {
+		Port string `env:"HTTP_PORT,required"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL,required"`
+	}
+
+	PG struct {
+		URL string `env:"PG_URL,required"`
+	}
+
+	Swagger struct {
+		Enabled bool `env:"SWAGGER_ENABLED" envDefault:"false"`
+	}
+)
+
+// NewConfig returns app config.
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	return cfg, nil
+}
